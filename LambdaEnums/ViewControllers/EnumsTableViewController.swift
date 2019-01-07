6e4a0139531d7dd7dd6625bcdebe5c7b4ba1f093
Abraham Hunt <abrahamhunt@gmail.com>
2019-01-07 13:07:51 -0700

    Code done during the live presentation

--- LambdaEnums/ViewControllers/EnumsTableViewController.swift
+++ LambdaEnums/ViewControllers/EnumsTableViewController.swift
@@ -14,6 +14,29 @@ class EnumsTableViewController: UITableViewController {
     var switchRows: [SwitchCellConfiguration] = [SwitchCellConfiguration]()
     var sliderRows: [SliderCellConfiguration] = [SliderCellConfiguration]()
 
+    enum TableSection {
+        case onSwitches
+        case sliders
+        case moods
+        
+        var title: String {
+            switch self {
+            case .onSwitches: return NSLocalizedString("Switches", comment: "Title for switches section")
+            case .sliders: return NSLocalizedString("Sliders", comment: "Title for sliders section")
+            case .moods: return NSLocalizedString("Moods", comment: "Title for moods section")
+            }
+        }
+    }
+    
+    enum TableRowType {
+        case onSwitch(SwitchCellConfiguration)
+        case slider(SliderCellConfiguration)
+        case text(TextCellConfiguration)
+    }
+    
+    var sections: [TableSection] = [.onSwitches, .sliders, .moods]
+    var rows = [TableRowType]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,43 +50,55 @@ class EnumsTableViewController: UITableViewController {
         
         let breakfast = SwitchCellConfiguration.init(isSwitchOn: true, title: NSLocalizedString("Breakfast", comment: "Title for the breakfast switch cell"))
         let lunch = SwitchCellConfiguration.init(isSwitchOn: false, title: NSLocalizedString("Lunch", comment: "Title for the lunch switch cell"))
-        switchRows = [breakfast, lunch]
+        let dinner = SwitchCellConfiguration.init(isSwitchOn: false, title: NSLocalizedString("Dinner", comment: "Title for the dinner switch cell"))
+        switchRows = [breakfast, lunch, dinner]
         
-        let calories = SliderCellConfiguration.init(title: NSLocalizedString("Calories", comment: "Title for the calories cell"), thumbColor: .red)
+        let calories = SliderCellConfiguration.init(title: NSLocalizedString("Calories", comment: "Title for the calories cell"), thumbColor: .colorWithRGB(red: 15, green: 74, blue: 138))
         let steps = SliderCellConfiguration.init(title: NSLocalizedString("Steps", comment: "Title for the steps cell"), thumbColor: .blue)
         sliderRows = [calories, steps]
+        
+        rows = moods.map({TableRowType.text($0)})
         
         tableView.reloadData()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+//        return sections.count
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0: return moods.count
-        case 1: return switchRows.count
-        case 2: return sliderRows.count
-        default: return 0
-        }
+//        let tableSection = sections[section]
+//        switch tableSection {
+//        case .onSwitches: return switchRows.count
+//        case .sliders: return sliderRows.count
+//        case .moods: return moods.count
+//        }
+        return rows.count
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 0: return NSLocalizedString("Moods", comment: "Title for moods section")
-        case 1: return NSLocalizedString("Sliders", comment: "Title for sliders section")
-        case 2: return NSLocalizedString("Switches", comment: "Title for switches section")
-        default: return nil
-        }
+        return nil
+//        let tableSection = sections[section]
+//        return tableSection.title
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.section {
-        case 0: return SwitchTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: switchRows[indexPath.row])
-        case 1: return SliderTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: sliderRows[indexPath.row])
-        case 2: return TextTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: moods[indexPath.row])
-        default: assert(false, "Shouldn't have cells in another section")
+//        let tableSection = sections[indexPath.section]
+//        switch tableSection {
+//        case .onSwitches: return SwitchTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: switchRows[indexPath.row])
+//        case .sliders: return SliderTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: sliderRows[indexPath.row])
+//        case .moods: return TextTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: moods[indexPath.row])
+//        }
+        
+        let tableRow = rows[indexPath.row]
+        switch tableRow {
+        case .onSwitch(let configuration):
+            return SwitchTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: configuration)
+        case .slider(let configuration):
+            return SliderTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: configuration)
+        case .text(let configuration):
+            return TextTableViewCell.dequeue(from: tableView, indexPath: indexPath, with: configuration)
         }
     }
     
